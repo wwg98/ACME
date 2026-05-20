92e2006797af92e56bb6dd760e8b1eec1526efa2
wwg98 <wwwg98@gmail.com>
2026-05-20 15:13:35 +0900

    feat 아코디언 수정

--- js/about.js
+++ js/about.js
@@ -3,23 +3,37 @@ const questions = document.querySelectorAll(".qnalist details");
 /*
   let c = a.getAttribute('title')
   a.setAttribute(b,c)  a에서b 속성의 값을 c로 변경
-
   대상.removeAttribute(속성명)
-
-  questions을 클릭하면 할일
-    클릭한 그 요소의 부모 찾고
-    부모의 형제 자매 찾고
-    클릭한 그 요소가 아닌 형제자매들에서 open 속성을 제거
-
+  속성의 존재 유무
+  대상.hasAttribute(속성명) true, false
 */
-questions.forEach(q => {
-  q.addEventListener("click", () => {
-    let parent = q.parentElement.parentElement;
-    let siblings = [...parent.children];
-    siblings.forEach(sibling => {
-      if (parent !== sibling) {
-        sibling.querySelector("details").removeAttribute("open");
-      }
+
+questions.forEach(question => {
+  question.addEventListener("click", e => {
+    e.preventDefault();
+    //open 상태 확인
+    let isOpen = question.hasAttribute("open");
+
+    // 모든 details에서 open 제거
+    questions.forEach(q => {
+      q.removeAttribute("open");
     });
+
+    //isOpen true이면 접고, isOpen false열기
+    if (!isOpen) {
+      question.setAttribute("open", "");
+    }
   });
 });
+
+// questions.forEach(q => {
+//   q.addEventListener("click", () => {
+//     let parent = q.parentElement.parentElement;
+//     let siblings = [...parent.children];
+//     siblings.forEach(sibling => {
+//       if (parent !== sibling) {
+//         sibling.querySelector("details").removeAttribute("open");
+//       }
+//     });
+//   });
+// });
